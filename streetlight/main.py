1f68bf2f3a420c69032deb9ad0a30aeac58a11de
Yves Schubert <yves.schubert@posteo.de>
2017-04-22 21:11:22 +0200

    Improved code for the streetlight (SUT)

--- streetlight/main.py
+++ streetlight/main.py
@@ -18,6 +18,7 @@ CHANNEL_OUT_LIGHT_SWITCH = 23
 
 ## METHODS ####################################################################
 
+
 def switch_light_on(cmdId):
     GPIO.output(CHANNEL_OUT_LIGHT_SWITCH, GPIO.HIGH)
     status = {
@@ -27,6 +28,7 @@ def switch_light_on(cmdId):
     }
     mqtt_client.publish(TOPIC_STREETLIGHT_STATUS + "/" + STREETLIGHT_ID, json.dumps(status))
     mqtt_client.publish(TOPIC_STREETLIGHT_STATUS, json.dumps(status))
+
 
 def switch_light_off(cmdId):
     GPIO.output(CHANNEL_OUT_LIGHT_SWITCH, GPIO.LOW)
@@ -40,11 +42,13 @@ def switch_light_off(cmdId):
 
 ## SETUP MQTT CLIENT ##########################################################
 
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_mqtt_connect(client, userdata, flags, rc):
     print("Connected to MQTT with result code " + str(rc))
     # listen for commands coming from the mqtt bus
     client.subscribe(TOPIC_STREETLIGHT_COMMAND + "/" + STREETLIGHT_ID)
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_mqtt_message(client, userdata, msg):
@@ -74,22 +78,38 @@ GPIO.setup(CHANNEL_IN_LIGHT_SENSOR, GPIO.IN)
 GPIO.setup(CHANNEL_OUT_LIGHT_SWITCH, GPIO.OUT)
 
 
-def onLightSensorFallingEdge():
+def init_gpios():
+    light_sensor_state = GPIO.input(CHANNEL_IN_LIGHT_SENSOR)
+    if (light_sensor_state == GPIO.HIGH):
+        GPIO.output(CHANNEL_OUT_LIGHT_SWITCH, GPIO.LOW)
+    else:
+        GPIO.output(CHANNEL_OUT_LIGHT_SWITCH, GPIO.HIGH)
+
+
+def on_light_sensor_low():
     print("It's getting dark => Switching on the light")
     switch_light_on("")
 
-def onLightSensorRisingEdge():
+
+def on_light_sensor_high():
     print("It's dawn => Switching off the light")
     switch_light_off("")
 
+init_gpios()
+
 # detect falling edge on light sensor (falling means: It's dark)
-GPIO.add_event_detect(CHANNEL_IN_LIGHT_SENSOR, GPIO.FALLING, callback=onLightSensorFallingEdge, bouncetime=200)
+GPIO.add_event_detect(CHANNEL_IN_LIGHT_SENSOR, GPIO.FALLING, callback=on_light_sensor_low, bouncetime=200)
 
 # detect rising edge on light sensor (rising means: It's bright outside)
-GPIO.add_event_detect(CHANNEL_IN_LIGHT_SENSOR, GPIO.RISING, callback=onLightSensorRisingEdge, bouncetime=200)
+GPIO.add_event_detect(CHANNEL_IN_LIGHT_SENSOR, GPIO.RISING, callback=on_light_sensor_high, bouncetime=200)
 
 
 mqtt_client.connect(MQTT_HOST, 1883, 60)
 
-# Process network traffic and dispatch callbacks.
-mqtt_client.loop_forever()
+try:
+    # Process network traffic and dispatch callbacks.
+    mqtt_client.loop_forever()
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
